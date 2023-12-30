a6617c1e4a9d517362869ce894947c5311da596f
Kauã Vitor Rodrigues <kvrfz.prsnl@gmail.com>
2023-12-30 20:11:20 -0300

    Create stage class for the scene
    Adds the `Stage` class responsible for managing dynamic interaction between characters within the game scene. This class receives two fighters (instances of the `Character` class) as parameters upon creation and starts the game, allowing attacks between them.
    
    Details of the changes:
    - Implementation of the constructor to set up the fighters within the scene.
    - `start()` method that initiates the game and sets up interaction through attack buttons for each fighter.
    - `update()` method to dynamically update life information and names of the characters in the HTML and adjust the life bar.
    - `doAttack()` method that simulates an attack between the characters.
    
    These changes enable basic interaction between characters within the game scene, visually updating information and allowing attacks between the fighters.

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -85,3 +85,59 @@ class BigMonster extends Character {
     }
 
 }
+
+// CLASSES DE CENÁRIO //
+
+// Cria a classe par interagir com o cenário dinamicamente
+class Stage {
+
+    /*
+        - Constructor que cria os lutadores
+        - Atribuí como lutadores os personagens (Criados pela classe Character) passados como parâmetro ao chamar a classe
+    */
+    constructor(fighter1, fighter2, fighter1EL, fighter2EL) {
+        this.fighter1 = fighter1
+        this.fighter2 = fighter2
+        this.fighter1EL = fighter1EL
+        this.fighter2EL = fighter2EL
+    }
+
+    // Função que executa os métodos de interação ao utilizar a classe (Inicia o jogo)
+    start() {
+
+        // Chama a função update
+        this.update() 
+
+        /*
+        
+            - Executa a função doAttack com base no clicar de um dos botões
+            - Passa o lutador relacionado ao botão de ataque pressionado como parâmetro para ser o causador do ataque
+            - Passa o outro lutador relacionado ao botão de ataque não pressionado como parâmetro para ser o sofredor do ataque
+
+        */
+        this.fighter1EL.querySelector('.button .attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2)) // Lutador da esquerda ataca
+        this.fighter2EL.querySelector('.button .attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1)) // Lutador da direita ataca
+    }
+
+    // Função que modifica o HTML e CSS da página dinamicamente
+    update() {
+
+        // Personagem 1
+        this.fighter1EL.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP` // Exibe o nome e a quantidade de vida do personagem 1
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife * 100) // Calcula a quantidade de vida do personagem 1 em porcentagem
+        this.fighter1EL.querySelector('.bar').style.width = `${f1Pct}%` // Define o tamanho da LifeBar (CSS) em relação a porcentagem de vida do personagem 1
+
+        // Personagem 2
+        this.fighter2EL.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP` // Exibe o nome e a quantidade de vida do personagem 2
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife * 100) // Calcula a quantidade de vida do personagem 2 em porcentagem
+        this.fighter2EL.querySelector('.bar').style.width = `${f2Pct}%` // Define o tamanho da LifeBar (CSS) em relação a porcentagem de vida do personagem 2
+
+    }
+
+    // Função que executa um ataque, tendo como parâmetro quem efetuou o ataque e quem recebeu
+    doAttack(attacking, attacked) {
+        console.log(`${attacking.name} ataca ${attacked.name}`)
+        this.update()
+    }
+
+}
